31cbec7992009a7039aab338eb199add1341beb1
sumitmakwana <sumitmakwanaa@gmail.com>
2024-07-01 17:01:20 +0530

    Necessary changes

--- trial1/script2.js
+++ trial1/script2.js
@@ -2,12 +2,3 @@ const scroll = new LocomotiveScroll({
     el: document.getElementById('main'),
     smooth: true
 });
-
-function CircleMouseFollower(){
-    window.addEventListener("mousemove", function(dets){
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-    })
-}
-
-
-CircleMouseFollower();
